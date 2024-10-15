c34f51b7e15afc91407c5d988237668da3117d94
Bala Karthik Venigalla <108982229+BalaKarthik07@users.noreply.github.com>
2024-10-15 06:24:01 +0530

    Create Chef and Instant Noodles.java

---
+++ Chef and Instant Noodles.java
@@ -0,0 +1,27 @@
+/*DATE: 15/10/2024
+
+Question:
+Chef and Instant Noodles
+
+
+
+My Code:
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		System.out.println(X*Y);
+	}
+}
+
