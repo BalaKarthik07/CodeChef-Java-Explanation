dd0e7eb99e6fef29906c6f5959ef2971af8d58e7
Bala Karthik Venigalla <108982229+BalaKarthik07@users.noreply.github.com>
2024-10-17 05:36:44 +0530

    Create Print Squares.java

---
+++ Print Squares.java
@@ -0,0 +1,27 @@
+/*
+
+DATE: 177/10/2024
+
+QUESTION:
+
+Print Squares
+Write a program that utilizes a while loop to print the squares of numbers from 1 to 5.
+
+  MY CODE:
+
+*/
+
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int x = 1;
+		while(x <=5){
+		    System.out.print((x*x)+" ");
+		    x++;
+		}
+
+	}
+}
