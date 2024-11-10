93dd3b7402fed6726759b4d5eac496147941d8c1
Bala Karthik Venigalla <108982229+BalaKarthik07@users.noreply.github.com>
2024-11-10 05:53:02 +0530

    Create Food Costs.java

---
+++ Food Costs.java
@@ -0,0 +1,47 @@
+/*
+
+
+DATE: 10/11/2024
+
+
+  QUESTION:
+
+
+
+Food Costs
+You're a bit all over the place as a college student. You used to eat out at expensive restaurants almost every day until your parents gave you a talking-to about being irresponsible. Now, you've got to control your eating and spending habits.
+
+So, here's the plan: you'll stick to the college mess for your meals every day, except Sundays. On Sundays, you're treating yourself to those fancy restaurant meals.
+The cost is Rs. X for the mess food each day, and Rs. Y for the restaurant splurges.
+
+Now, what's the cost of food per week? Note that you don't have to pay for the mess on Sundays.
+A week has seven days, as usual.
+
+Input Format
+The first and only line of input contains 2 space-separated integers, X and Y.
+  
+Output Format
+Output a single integer: the weekly cost of food.
+
+
+  MY CODE:
+
+*/
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s = new Scanner(System.in);
+		int X = s.nextInt();
+		int Y = s.nextInt();
+		System.out.println((X*6)+(1*Y));
+
+	}
+}
